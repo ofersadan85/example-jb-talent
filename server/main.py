f6fa28da3401c910ed637bbfb54a76689b102a69
Ofer Sadan <ofersadan85@gmail.com>
2026-03-12 15:54:51 +0200

    more server examples

--- server/main.py
+++ server/main.py
@@ -1,9 +1,17 @@
-from fastapi import FastAPI
-from models import User
 from pathlib import Path
 
+from fastapi import FastAPI, Request
+from models import User
+
 app = FastAPI()
+app.state.count = 0
 DEFAULT_USERS_DIR = Path(__file__).parent / "users"
+
+
+@app.get("/count")
+def count(request: Request) -> int:
+    request.app.state.count += 1
+    return request.app.state.count
 
 
 @app.get("/users")
@@ -11,6 +19,11 @@ def list_users() -> list[User]:
     return [User.model_validate_json(f.read_text()) for f in DEFAULT_USERS_DIR.glob("*.json")]
 
 
-@app.get("/user")
+@app.get("/users/{id}")
 def get_user(id: int) -> User:
     return User.load_from_file(DEFAULT_USERS_DIR, id)
+
+
+@app.get("/users/{id}/{field_name}")
+def get_user_email(id: int, field_name: str) -> str:
+    return User.load_from_file(DEFAULT_USERS_DIR, id).model_dump()[field_name]
