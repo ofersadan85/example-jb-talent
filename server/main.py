3c9e9ff19120e0635bd20297109fceb15c174a33
Ofer Sadan <ofersadan85@gmail.com>
2026-03-12 14:35:14 +0200

    example server with users routes

--- server/main.py
+++ server/main.py
@@ -1,19 +1,16 @@
 from fastapi import FastAPI
-
+from models import User
+from pathlib import Path
 
 app = FastAPI()
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+DEFAULT_USERS_DIR = Path(__file__).parent / "users"
 
 
 @app.get("/users")
-def list_users():
-    return ["ofer", "maya", "avi", "dana"]
+def list_users() -> list[User]:
+    return [User.model_validate_json(f.read_text()) for f in DEFAULT_USERS_DIR.glob("*.json")]
 
 
-@app.post("/users")
-def create_user():
-    pass
+@app.get("/user")
+def get_user(id: int) -> User:
+    return User.load_from_file(DEFAULT_USERS_DIR, id)
---
+++ server/models.py
@@ -0,0 +1,66 @@
+from pathlib import Path
+
+import httpx
+from pydantic import BaseModel
+
+USERS_URL_PATH = "/users/"
+USERS_FULL_URL = "https://jsonplaceholder.typicode.com/" + USERS_URL_PATH
+
+
+class UserAddress(BaseModel):
+    street: str
+    city: str
+
+
+class UserCompany(BaseModel):
+    name: str
+    catchPhrase: str
+    bs: str
+
+
+class User(BaseModel):
+    id: int | None
+    name: str
+    username: str
+    email: str
+    company: UserCompany | None = None
+    address: UserAddress | None = None
+
+    def save_to_file(self, users_dir: Path):
+        file_path = users_dir / (str(self.id) + ".json")
+        file_path.write_text(self.model_dump_json(indent=2))
+
+    def save_to_web(self, client: httpx.Client | None = None) -> "User":
+        user_dump = self.model_dump(exclude={"id"}, exclude_none=True)
+        if client is None:
+            response = httpx.post(USERS_FULL_URL, json=user_dump)
+        else:
+            response = client.post(USERS_URL_PATH, json=user_dump)
+        return User.model_validate(response.json())
+
+    @classmethod
+    def load_from_file(cls, users_dir: Path, id: int) -> "User":
+        file_path = users_dir / (str(id) + ".json")
+        data = file_path.read_text()
+        return cls.model_validate_json(data)
+
+    @classmethod
+    def get_from_web(cls, id: int, client: httpx.Client | None = None) -> "User":
+        if client is None:
+            response = httpx.get(USERS_FULL_URL + str(id))
+        else:
+            response = client.get(USERS_URL_PATH + str(id))
+        return cls.model_validate(response.json())
+
+    @classmethod
+    def get_multiple_from_web(
+        cls, ids: list[int] | None = None, client: httpx.Client | None = None
+    ) -> list["User"]:
+        if ids is None:
+            if client is None:
+                response = httpx.get(USERS_FULL_URL)
+            else:
+                response = client.get(USERS_URL_PATH)
+            return [cls.model_validate(u) for u in response.json()]
+        else:
+            return [cls.get_from_web(i, client) for i in ids]
