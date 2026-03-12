696885496caa859ea5e57207b4255fbbd9311603
Ofer Sadan <ofersadan85@gmail.com>
2026-03-12 10:10:50 +0200

    completed logging example

--- log.py
+++ log.py
@@ -1,31 +1,49 @@
 import logging
+import sys
+
 import httpx
 
-log_fmt = "%(asctime)s %(levelname)s (%(filename)s:%(funcName)s:%(lineno)d) %(message)s"
 # logging.basicConfig(level=logging.DEBUG, filename="myapp.log", format=log_fmt)
 logger = logging.getLogger("myapp")
-logger.setLevel(logging.DEBUG)
-print_handler = logging.StreamHandler()
-print_handler.setLevel(logging.DEBUG)
-logger.addHandler(print_handler)
+
+
+def setup_logging():
+    logger.setLevel(logging.ERROR)
+
+    if not logger.handlers:
+        print_handler = logging.StreamHandler(sys.stdout)
+        print_handler.setLevel(logging.INFO)
+        print_fmt = logging.Formatter("%(levelname)s %(message)s")
+        print_handler.setFormatter(print_fmt)
+
+        file_handler = logging.FileHandler("myapp.log")
+        file_handler.setLevel(logging.DEBUG)
+        file_fmt = logging.Formatter(
+            "%(asctime)s %(levelname)s (%(filename)s:%(funcName)s:%(lineno)d) %(message)s"
+        )
+        file_handler.setFormatter(file_fmt)
+
+        logger.addHandler(print_handler)
+        logger.addHandler(file_handler)
+
 
 # message
 #    |
 #   logger("myapp")
 #            |
 #            -----> print_handler
+#            |
+#            -----> file_handler
 
 
 def google():
+    print("Normal print")
     logger.info("We are checking google")
     httpx.get("https://www.google.com")
     logger.info("Done checking google")
-  
 
 
-logger.error("This is an error")
-logger.warning("This is a warning")
-logger.info("This is some info")
-logger.debug("This is for debug mode only")
-
-google()
+if __name__ == "__main__":
+    setup_logging()
+    google()
+    logger.info("Test")
